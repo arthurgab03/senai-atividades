c3dad67530bfdc42a0150a615039b32d5ee5313b
Arthur Gabriel <arthurgabriel120703@gmail.com>
2023-05-06 20:56:01 -0300

    projeto gamemania

---
+++ gamemania/js/script.js
@@ -0,0 +1,32 @@
+//efeito do botão voltar ao Topo
+
+function topo(){
+    window.scrollTo(
+        {
+            top: 0, 
+            left: 0,
+            behavior:'smooth'
+        }
+    )
+}
+
+//Validação de Login
+
+function login(){
+    var usuario = document.getElementById('usuario').value;
+    var senha = document.getElementById('senha').value;
+    var erro = document.querySelector('.login-erro');
+
+    if(usuario == 'admin' && senha == 'Arthur1@n'){
+        window.location = 'index.html';
+    } else {
+        erro.classList.add('mostra');   
+        
+    }
+}
+
+//Ativar alert no botão cadastrar
+
+function cadastro(){
+    window.location = 'logado.html'
+}
